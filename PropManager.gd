263669fc39b1c4ffb656f81b3d264e9bc37a5d3b
Chris <32988106+Noobot9k@users.noreply.github.com>
2024-08-19 15:20:17 -0600

    added design test level

--- PropManager.gd
+++ PropManager.gd
@@ -10,7 +10,7 @@ static func GetVolumeOfBox(boundingBox : Vector3):
 	return boundingBox.x * boundingBox.y * boundingBox.z
 
 static func GetVolumeOfObject(obj : RigidBody3D):
-	for shape : VisualInstance3D in obj.get_children():
+	for shape in obj.get_children():
 		if not shape.is_class("VisualInstance3D"): continue
 		var boundingBox : AABB = shape.get_aabb()
 		return boundingBox.get_volume()
